614a25129c47f364ab53272a9827e56301cc0300
Kathir7788 <143430553+Kathir7788@users.noreply.github.com>
2023-10-03 16:51:58 +0530

    Add files via upload

---
+++ even numbers.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf(" enter value of n:");
+scanf("%d",n);
+printf("odd numbers are :");
+for (int i=0,i < n,i=i+2);
+{
+	printf("%d",i);
+}
+return 0;
+}
